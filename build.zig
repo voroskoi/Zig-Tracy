9d09b931f3d0b9b2f32ad6f87b115241821fdf35
VÖRÖSKŐI András <voroskoi@gmail.com>
2023-09-17 09:20:22 +0200

    test

--- build.zig
+++ build.zig
@@ -6,19 +6,9 @@ const LibExeObjStep = std.Build.LibExeObjStep;
 pub fn build(b: *Builder) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
-    const enable = b.option(bool, "enable_tracy", "Enable profiling with Tracy") orelse false;
-
-    const options = b.addOptions();
-    options.addOption(bool, "tracy_enabled", enable);
 
     _ = b.addModule("tracy", .{
         .source_file = .{ .path = "tracy.zig" },
-        .dependencies = &.{
-            .{
-                .name = "build_options",
-                .module = options.createModule(),
-            },
-        },
     });
 
     const lib = b.addStaticLibrary(.{
@@ -28,27 +18,25 @@ pub fn build(b: *Builder) void {
         .optimize = optimize,
     });
     b.installArtifact(lib);
-    if (enable) {
-        lib.addIncludePath(.{ .path = "tracy" });
-        lib.addCSourceFile(.{
-            .file = .{ .path = "tracy/TracyClient.cpp" },
-            // MinGW doesn't have all the newfangled windows features,
-            // so we need to pretend to have an older windows version.
-            .flags = &.{
-                "-DTRACY_ENABLE",
-                "-D_WIN32_WINNT=0x601",
-                "-fno-sanitize=undefined",
-            },
-        });
+    lib.addIncludePath(.{ .path = "tracy" });
+    lib.addCSourceFile(.{
+        .file = .{ .path = "tracy/TracyClient.cpp" },
+        // MinGW doesn't have all the newfangled windows features,
+        // so we need to pretend to have an older windows version.
+        .flags = &.{
+            "-DTRACY_ENABLE",
+            "-D_WIN32_WINNT=0x601",
+            "-fno-sanitize=undefined",
+        },
+    });
 
-        lib.linkLibC();
-        lib.linkLibCpp();
+    lib.linkLibC();
+    lib.linkLibCpp();
 
-        if (lib.target.isWindows()) {
-            lib.linkSystemLibrary("Advapi32");
-            lib.linkSystemLibrary("User32");
-            lib.linkSystemLibrary("Ws2_32");
-            lib.linkSystemLibrary("DbgHelp");
-        }
+    if (lib.target.isWindows()) {
+        lib.linkSystemLibrary("Advapi32");
+        lib.linkSystemLibrary("User32");
+        lib.linkSystemLibrary("Ws2_32");
+        lib.linkSystemLibrary("DbgHelp");
     }
 }
